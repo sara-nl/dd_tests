11c76f7f5e4b28ce416735600326a6b02f2affbb
Ron Trompert <ron.trompert@surf.nl>
2024-11-01 17:45:11 +0100

    now handles absent output files

--- result.sh
+++ result.sh
@@ -5,41 +5,50 @@
 export PATH=/usr/local/bin:$PATH
 
 tpr=0
-for file in ioread*
-do 
-seconds=`cat $file | grep 'bytes copied' | awk '{print $7}'`
-bytes=$FILESIZE
+list=`ls -1 ioread* 2>/dev/null`
+if [ "${list}"!="x" ]; then
 
-i=0
-ts=0
-for s in $seconds
-do 
-   i=`expr $i + 1`
-   ts=`${PYTHON} -c "print (str($s+$ts))"`
-done
-t=`${PYTHON} -c "print (str($i*$bytes/($ts*1024*1024)))"`
-echo "read: $t"
-tpr=`${PYTHON} -c "print (str($t+$tpr))"`
-done
+    for file in ioread*
+    do 
+    seconds=`cat $file | grep 'bytes copied' | awk '{print $7}'`
+    bytes=$FILESIZE
+
+    i=0
+    ts=0
+    for s in $seconds
+    do 
+       i=`expr $i + 1`
+       ts=`${PYTHON} -c "print (str($s+$ts))"`
+    done
+    t=`${PYTHON} -c "print (str($i*$bytes/($ts*1024*1024)))"`
+    echo "read: $t"
+    tpr=`${PYTHON} -c "print (str($t+$tpr))"`
+    done
+
+fi
 
 tpw=0
+list=`ls -1 iowrite* 2>/dev/null`
+if [ "${list}"!="x" ]; then
 
-for file in iowrite*
-do
-seconds=`cat $file | grep 'bytes copied' | awk '{print $7}'`
-bytes=`cat $file | grep -m 1 'bytes copied' | awk '{print $3}'`
+    for file in iowrite*
+    do
+    seconds=`cat $file | grep 'bytes copied' | awk '{print $7}'`
+    bytes=`cat $file | grep -m 1 'bytes copied' | awk '{print $3}'`
 
-i=0
-ts=0
-for s in $seconds
-do 
-   i=`expr $i + 1`
-   ts=`${PYTHON} -c "print (str($s+$ts))"`
-done
-t=`${PYTHON} -c "print (str($i*$bytes/($ts*1024*1024)))"`
-echo write: $t
-tpw=`${PYTHON} -c "print (str($t+$tpw))"`
-done
+    i=0
+    ts=0
+    for s in $seconds
+    do 
+       i=`expr $i + 1`
+       ts=`${PYTHON} -c "print (str($s+$ts))"`
+    done
+    t=`${PYTHON} -c "print (str($i*$bytes/($ts*1024*1024)))"`
+    echo write: $t
+    tpw=`${PYTHON} -c "print (str($t+$tpw))"`
+    done
+
+fi
 
 echo avg throughput reads: $tpr MiB/s
 echo avg throughput writes: $tpw MiB/s
